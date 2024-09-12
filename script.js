66ce0a377dea3ee9badab1f7e15e45771cc86498
tubi55 <tubi55@nate.com>
2024-09-12 11:36:51 +0900

    다양한 조건문 형태

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+console.log("test");
 /*
   조건문 
   - 비교연산자를 활용한 조건식을 이용해서 특정 조건일때 서로 다른 코드가 실행되도록 분기처리
@@ -14,3 +15,33 @@
     위의 모든 조건조건이 참이 아닐때 이곳의 구문을 무조건 실행하고 종료
   }
 */
+
+const hour = new Date().getHours();
+console.log(hour);
+
+//0~9 :lightblue
+//10~15 : orange
+//16~20 : pink
+//21~24 : purple
+
+/*
+if (hour >= 0 && hour < 10) {
+  document.body.style.backgroundColor = "lightblue";
+} else if (hour >= 10 && hour < 16) {
+  document.body.style.backgroundColor = "orange";
+} else if (hour >= 16 && hour < 21) {
+  document.body.style.backgroundColor = "pink";
+} else {
+  document.body.style.backgroundColor = "purple";
+}
+*/
+
+// if (hour >= 0 && hour < 10) document.body.style.backgroundColor = "lightblue";
+// else if (hour >= 10 && hour < 16) document.body.style.backgroundColor = "orange";
+// else if (hour >= 16 && hour < 21) document.body.style.backgroundColor = "pink";
+// else document.body.style.backgroundColor = "purple";
+
+if (hour >= 0 && hour < 10) document.body.style.backgroundColor = "lightblue";
+if (hour >= 10 && hour < 16) document.body.style.backgroundColor = "orange";
+if (hour >= 16 && hour < 21) document.body.style.backgroundColor = "pink";
+if (hour >= 21 && hour < 25) document.body.style.backgroundColor = "purple";
